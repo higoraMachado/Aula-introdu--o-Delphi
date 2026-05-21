30b2c976d3e2089f31ed609a388fe9c627c5e3d7
Higor Machado <higor.machado2011@gmail.com>
2026-05-20 21:23:53 -0300

    .

--- Componentes.dpr
+++ Componentes.dpr
@@ -17,7 +17,8 @@ uses
   UAtividade48 in 'UAtividade48.pas' {frmAtividade48},
   UAtividade49 in 'UAtividade49.pas' {frmAtividade49},
   UAtividade50 in 'UAtividade50.pas' {frmAtividade50},
-  UAtividade51 in 'UAtividade51.pas' {frmAtividade51};
+  UAtividade51 in 'UAtividade51.pas' {frmAtividade51},
+  UAtividade52 in 'UAtividade52.pas' {frmAtividade52};
 
 {$R *.res}
 
@@ -40,5 +41,6 @@ begin
   Application.CreateForm(TfrmAtividade49, frmAtividade49);
   Application.CreateForm(TfrmAtividade50, frmAtividade50);
   Application.CreateForm(TfrmAtividade51, frmAtividade51);
+  Application.CreateForm(TfrmAtividade52, frmAtividade52);
   Application.Run;
 end.
